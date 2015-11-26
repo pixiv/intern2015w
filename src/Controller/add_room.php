f135a77168e25f1ed078a089f1839cdce3b04b44
Yosuke INOUE <inoue@kamikakushi.net>
2015-11-27 03:19:27 +0900

    room作成作業を一つのTransactionで行うようにした

--- src/Controller/add_room.php
+++ src/Controller/add_room.php
@@ -1,6 +1,7 @@
 <?php
 namespace Nyaan\Controller;
 use Baguette\Response;
+use PDO;
 
 /**
  * @package   Nyaan\Controller
@@ -12,10 +13,8 @@ final class add_room
 {
     function action(\Baguette\Application $app, \Teto\Routing\Action $action)
     {
-        $is_daburi = self::isTyouhuku(isset($_REQUEST['slug']) ?? '');
-
-        if (!$is_daburi && isset($_REQUEST['slug'], $_REQUEST['name'])
-            && self::regist($_REQUEST['slug'], $_REQUEST['name'], $app->getLoginUser())
+        if (isset($_REQUEST['slug'], $_REQUEST['name'])
+            && self::create_room($_REQUEST['slug'], $_REQUEST['name'], $app->getLoginUser())
         ) {
             return new Response\RedirectResponse('/rooms/' . $_REQUEST['slug']);
         }
@@ -23,14 +22,62 @@ final class add_room
         return new Response\RedirectResponse('/');
     }
 
-    private static function isTyouhuku(string $slug): bool
+    /**
+     * create new room
+     *
+     * @param string room_slug 作成するroomのslug
+     * @param string room_name 作成するroomのname
+     * @param ? admin_user 作成するroomのadmin
+     * @return bool 成功したならtrue, 失敗したなら false
+     */
+    private static function create_room(
+        string $room_slug,
+        string $room_name,
+        $user): bool
+    {
+        if (!self::is_unique($room_slug))
+            return false;
+
+        $query = array(
+            'create_room' => 'INSERT INTO `rooms`(`slug`, `name`) VALUES(:slug, :name);',
+            'system_post' => 'INSERT INTO `posts` VALUES(:id, 0, :date, :message);'
+        );
+
+        try {
+            db()->beginTransaction();
+
+            $stmt = db()->prepare($query['create_room']);
+            $stmt->bindValue(':slug', $room_slug, PDO::PARAM_STR);
+            $stmt->bindValue(':name', $room_name, PDO::PARAM_STR);
+            $stmt->execute();
+
+            $room_id = db()->lastInsertId();
+            $now = date('Y-m-d H:i:s', strtotime('+9 hours'));
+            $message = "**{$user->name}さん**が部屋を作りました！";
+
+            $stmt = db()->prepare($query['system_post']);
+            $stmt->bindValue(':id', $room_id, PDO::PARAM_INT);
+            $stmt->bindValue(':date', $now, PDO::PARAM_STR);
+            $stmt->bindValue(':message', $message, PDO::PARAM_STR);
+            $stmt->execute();
+
+            db()->commit();
+        } catch (PDOException $e) {
+            db()->rollback();
+            return false;
+        }
+
+        return true;
+    }
+
+    private static function is_unique(string $slug): bool
     {
         $query = 'SELECT * FROM `rooms` WHERE `slug` = ?;';
         $stmt = db()->prepare($query);
         $stmt->execute([$slug]);
         $data = $stmt->fetch(\PDO::FETCH_ASSOC);
 
-        return !empty($data);
+        return empty($data);
     }
 
     private static function regist($slug, $name, $user): bool
