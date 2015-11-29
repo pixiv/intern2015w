e863d9dc1de530f48daf3e2d253928001a804885
Ryosuke Sato <rskjtwp@gmail.com>
2015-11-29 23:58:53 +0900

    refs #10: /@slug にTwitterやWebサイトの表示をする Close #10

--- src/Controller/user.php
+++ src/Controller/user.php
@@ -13,9 +13,19 @@ final class user
     public function action(\Baguette\Application $app, \Teto\Routing\Action $action)
     {
         $name = ltrim($action->param['user'], '@');
-        $query = "SELECT * FROM `users` WHERE `slug` = ?";
+        $query = "SELECT user_id FROM `users` WHERE `slug` = ?";
         $stmt = db()->prepare($query, array('text'));
         $stmt->execute(array($name));
+        $id = $stmt->fetch(\PDO::FETCH_ASSOC);
+        $id = $id['user_id'];
+
+        $query =
+               "SELECT * FROM users " .
+               "LEFT JOIN user_profile ".
+               "ON user_profile.user_id = users.user_id " .
+               "WHERE users.user_id = ?";
+        $stmt = db()->prepare($query);
+        $stmt->execute(array(intval($id)));
         $user = $stmt->fetch(\PDO::FETCH_ASSOC);
 
         if ($user) {
