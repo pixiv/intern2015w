e3e25e55e6fad666bfc18ef7701209d8ec7c3dc5
datake <sigur.ku@gmail.com>
2015-11-30 18:24:44 +0900

    remove unnecessary escape

--- src/Controller/room.php
+++ src/Controller/room.php
@@ -22,7 +22,7 @@ final class room
 
         if (!empty($_REQUEST['message'])) {
             $now = date('Y-m-d H:i:s', strtotime('+9 hours'));
-            $message = str_replace('"', '\\"', $_REQUEST['message']);
+            $message = $_REQUEST['message'];
             $user_id = $_REQUEST['user_id'];
             $query = "INSERT INTO `posts` VALUES( {$data['id']}, :user_id, \"{$now}\", :message )";
             $stmt = db()->prepare($query);
