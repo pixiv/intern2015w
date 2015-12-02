264b83addf85581fd59c81b61415d231ca721af4
Yuji Tanimoto <y.tanimoto13@gmail.com>
2015-12-03 08:27:06 +0900

    新規登録の重複判定でログイン名を渡すように変更

--- src/Controller/regist.php
+++ src/Controller/regist.php
@@ -11,7 +11,7 @@ class regist
             return new Response\RedirectResponse('/');
         }
 
-        $is_daburi = self::isTyouhuku($_REQUEST['user'] ?? '');
+        $is_daburi = self::isTyouhuku($_REQUEST['slug'] ?? '');
 
         if (!isset($_REQUEST['user'], $_REQUEST['slug'], $_REQUEST['password'], $_REQUEST['password_confirmation'])) {
             return new Response\TwigResponse('regist.tpl.html', [
